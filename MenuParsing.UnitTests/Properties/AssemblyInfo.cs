8cb0d35edf35420f297fa868295bdd046dc964b8
Jared Pinchot <jmpinchot@gmail.com>
2018-02-26 13:01:05 -0800

    added unit tests

--- MenuParsing.UnitTests/Properties/AssemblyInfo.cs
+++ MenuParsing.UnitTests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MenuParsing")]
+[assembly: AssemblyTitle("MenuParsing.UnitTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MenuParsing")]
+[assembly: AssemblyProduct("MenuParsing.UnitTests")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("853de63c-e414-4ebd-b516-cc7ddf0b42a6")]
+[assembly: Guid("d40daa07-4964-4340-a10d-57b7e8b7f25f")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -34,5 +34,3 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-
-[assembly: InternalsVisibleTo("MenuParsing.UnitTests")]
